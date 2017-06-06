fa40eb7c1c52b39540925aecbd37d1e0bc035135
cholhong <imich@nate.com>
2017-06-06 14:39:53 +0900

    9-1

---
+++ Lab9-1/src/lab/JDBCExample.java
@@ -0,0 +1,55 @@
+package lab;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCExample {
+
+	public static void main(String[] args) {
+
+		try {
+				Class.forName("com.mysql.jdbc.Driver");
+				System.out.println("Driver Loaded");
+			} catch (ClassNotFoundException e) {
+				System.out.println("Can not find driver class");
+			}
+			
+			Connection conn = null;
+			try {
+				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/com", "com", "com01" );
+				System.out.println("connected");
+			
+				int[] id = { 1, 2, 3 };
+				String[] name = { "KIM", "PARK", "LEE" };
+				String[] addr = { "A", "B", "C" };
+			
+				String sql = "INSERT INTO customer (id, name, addr) VALUES ( ?, ?, ? )";
+			
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+			
+				for(int i=0; i < id.length; i++) {
+					pstmt.setInt(1, id[i]);
+					pstmt.setString(2, name[i]);
+					pstmt.setString(3, addr[i]);
+					pstmt.executeUpdate();
+				}
+				
+			Statement stmt = conn.createStatement();
+			ResultSet rset = stmt.executeQuery("SELECT id, name, addr FROM customer");
+			
+			while(rset.next()) {
+				System.out.println("id: " + rset.getInt(1));
+				System.out.println("name: " + rset.getString(2));
+				System.out.println("addr: " + rset.getString(3));
+			}
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			} finally {
+				if(conn != null) try {conn.close();} catch (SQLException e) {}
+			}
+	}
+}
