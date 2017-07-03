a863695a676a46433c447db24ede2c28a2cb5e60
cholhong <imich@nate.com>
2017-07-03 15:37:54 +0900

    XMLDAO add

---
+++ Lab6-1/src/main/java/lab/CustomerXMLDAO.java
@@ -0,0 +1,9 @@
+package lab;
+public class CustomerXMLDAO {
+      public String getCustName(String id) {
+            return id + " eGovFrame XML";
+      }
+      public String getCustGrade(String id) {
+            return id + " S XML";
+      }
+}
